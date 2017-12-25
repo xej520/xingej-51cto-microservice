b33e8c80aba3534965b18c9a51cdb149a2d581dd
邢尔军 <xingej@163.com>
2017-12-25 20:03:30 +0800

    更新名字，更新依赖

--- microservice-discovery-eureka/src/main/java/com/itmuch/cloud/EurekaApplication.java
+++ microservice-discovery-eureka/src/main/java/com/itmuch/cloud/EurekaApplication.java
@@ -2,14 +2,16 @@ package com.itmuch.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
-public class App {
+@EnableEurekaServer
+public class EurekaApplication {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(EurekaApplication.class, args);
     }
 }
