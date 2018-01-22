92241b2124deb969e759bc776ee91235bd1e0ea1
邢尔军 <xingej@163.com>
2018-01-22 18:47:44 +0800

    服务提供者：将服务注册到Eureka上，分3步：添加依赖，添加注解，更新配置文件

--- microservice-provider-user/src/main/java/com/itmuch/cloud/MicroserviceSimpleProviderUserApplication.java
+++ microservice-provider-user/src/main/java/com/itmuch/cloud/MicroserviceSimpleProviderUserApplication.java
@@ -2,8 +2,13 @@ package com.itmuch.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+
+//将服务注册到Eureka上，第二步
+@EnableEurekaClient
 public class MicroserviceSimpleProviderUserApplication {
 
 	public static void main(String[] args) {
