bd19bb9e652170929958f079f2a214186fc4239c
邢尔军 <xingej@163.com>
2018-01-23 08:59:40 +0800

    测试EurekaClient和 DiscoveryClient

--- microservice-provider-user/src/main/java/com/itmuch/cloud/controller/UserController.java
+++ microservice-provider-user/src/main/java/com/itmuch/cloud/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.itmuch.cloud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.itmuch.cloud.entity.User;
 import com.itmuch.cloud.repository.UserRepository;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 
 @RestController
 public class UserController {
@@ -14,6 +18,12 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private EurekaClient eurekaClient;
+    
+    @Autowired
+    private DiscoveryClient discoveryClient;
+    
     @GetMapping(value = "/simple/{id}")
     public User findById(@PathVariable Long id) {
 
@@ -21,4 +31,17 @@ public class UserController {
 
     }
 
+    @GetMapping("/eureka-instance")
+    public String serviceUrl() {
+        InstanceInfo instance = eurekaClient.getNextServerFromEureka("MICROSERVICE-PROVIDER-USER", false);
+        return instance.getHomePageUrl();
+    }
+    
+    @GetMapping("/instance-info")
+    public ServiceInstance showInfo(){
+        ServiceInstance localServiceInstance = this.discoveryClient.getLocalServiceInstance();
+        
+        return localServiceInstance;
+    }
+    
 }
