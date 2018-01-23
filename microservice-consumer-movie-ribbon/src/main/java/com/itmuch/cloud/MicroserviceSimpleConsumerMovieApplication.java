e99386ad74b62f19d96479a07aa4505b8e1a998f
邢尔军 <xingej@163.com>
2018-01-23 14:07:05 +0800

    从movie工程，copy一份，改成ribbon工程，目的是负载均衡，这样的话，配置文件里，就不用将服务的IP地址写死了。

---
+++ microservice-consumer-movie-ribbon/src/main/java/com/itmuch/cloud/MicroserviceSimpleConsumerMovieApplication.java
@@ -0,0 +1,32 @@
+package com.itmuch.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MicroserviceSimpleConsumerMovieApplication {
+
+    // 方法名：restTemplate
+    // 最好跟MovieController里的 private RestTemplate restTemplate;
+    // restTemplate 保持一致哦
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(MicroserviceSimpleConsumerMovieApplication.class, args);
+
+        String[] allBeanNames = context.getBeanDefinitionNames();
+
+        for (String key : allBeanNames) {
+            System.out.println("-------->:\t" + key);
+        }
+
+    }
+}
