48fc57fa1d2ef88f7478b196980eba29b42d9f8b
邢尔军 <xingej@163.com>
2018-01-23 09:02:13 +0800

    消费者消费Eureka上服务的步骤，3步：添加依赖，添加注解，更新配置文件

--- microservice-consumer-movie/src/main/java/com/itmuch/cloud/MicroserviceSimpleConsumerMovieApplication.java
+++ microservice-consumer-movie/src/main/java/com/itmuch/cloud/MicroserviceSimpleConsumerMovieApplication.java
@@ -2,11 +2,13 @@ package com.itmuch.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroserviceSimpleConsumerMovieApplication {
 
     // 方法名：restTemplate
